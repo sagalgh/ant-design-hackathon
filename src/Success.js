08cb313f7264887590745b671be0ffa485b2ab9e
sagalgh <89239611+sagalgh@users.noreply.github.com>
2022-05-05 10:34:39 -0400

    first commit

--- src/Success.js
+++ src/Success.js
@@ -5,7 +5,7 @@ import {Space, Card, PageHeader} from 'antd'
 const Success = () => {
   return (
     <Space align="center" direction="vertical" size="middle" style={{ display: 'flex' }}> 
-    <div className="space-align-container">
+    <div className="space-align-container-success">
     <Card size="large" style={{ width: 350, boxShadow: "5px 8px 24px 5px rgba(208, 216, 243, 0.6)" }}>
    <div>
     <img src={logo} alt="League logo"/>
@@ -13,7 +13,7 @@ const Success = () => {
     <div className='success-vector'>
     <PageHeader title={"Signed in!"}/>
     
-    <img src={vector} alt="League logo"/>
+    <img src={vector} alt="Signed In vector" width="103" height="103"/>
     </div>
 </Card>
  </div>
